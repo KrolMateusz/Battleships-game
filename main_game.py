38479bdd7ddf737c01c1e9f6ad03e61b6247ae9f
Mateusz Król <matek8952@gmail.com>
2019-10-07 20:35:37 +0200

    Ships placing on board

--- main_game.py
+++ main_game.py
@@ -1,4 +1,4 @@
-def generate_map(size):
+def generate_empty_map(size):
     battlefield = {}
     for letter in 'ABCDEFGHIJ':
         battlefield.setdefault(letter, ["." for _ in range(size)])
@@ -18,5 +18,29 @@ def print_map(battlefield):
         print()
 
 
+def generate_ships():
+    ships = {
+        5: 1,
+        4: 1,
+        3: 1,
+        2: 1,
+        1: 1,
+    }
+    return ships
+
+
+def place_ships(ships_to_place):
+    while ships_to_place:
+        for ship_size in ships_to_place:
+            ship_placed = 0
+            while ship_placed < ships_to_place[ship_size]:
+                for ship_frag in range(ship_size):
+                    print(ship_frag)
+                # Walidacja
+                ship_placed += 1
+        ships_to_place.pop(ship_size)
+
+
 if __name__ == "__main__":
-    print_map(generate_map(10))
+    print_map(generate_empty_map(10))
+    place_ships(generate_ships())

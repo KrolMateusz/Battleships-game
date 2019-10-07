ee5588ad36842751fbf66748e05a48a3f929805b
Mateusz Król <matek8952@gmail.com>
2019-10-07 17:54:47 +0200

    Printing empty map

--- main_game.py
+++ main_game.py
@@ -5,5 +5,18 @@ def generate_map(size):
     return battlefield
 
 
+def print_empty_map(battlefield):
+    print(end=' ')
+    for number in range(len(battlefield)):
+        print(number  + 1, end=' ')
+        if number + 1 == len(battlefield):
+            print()
+    for row in battlefield:
+        print(row, end='')
+        for _ in range(len(battlefield)):
+            print('.', end=' ')
+        print()
+
+
 if __name__ == "__main__":
-    print(generate_map(10))
+    print_empty_map(generate_map(10))

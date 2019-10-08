26731b90be2fb4f98d8f2c17cd1f2820099d9754
Mateusz Król <matek8952@gmail.com>
2019-10-08 10:09:10 +0200

    Ship coords added

--- main_game.py
+++ main_game.py
@@ -30,23 +30,43 @@ def generate_ships():
     return ships
 
 
-def check_arrangement(ship_start, ship_end, ship_len):
+def check_ship(ship_start, ship_end, ship_len):
     alphabet = string.ascii_uppercase
     ship_start_letter, ship_start_number = ship_start
     ship_start_letter_index = alphabet.index(ship_start_letter)
     ship_end_letter, ship_end_number = ship_end
     ship_end_letter_index = alphabet.index(ship_end_letter)
-    # Walidacja pozioma
-    if ship_start_number == ship_end_number:
-        if (ship_start_letter_index - ship_end_letter_index == ship_len - 1 or 
-                ship_start_letter_index - ship_end_letter_index == -ship_len + 1):
-            return True
     # Walidacja pionowa
+    if ship_start_number == ship_end_number:
+        if (ship_start_letter_index - ship_end_letter_index == ship_len - 1 or
+                ship_start_letter_index - ship_end_letter_index == -ship_len + 1):
+            return 'vertical'
+    # Walidacja pozioma
     if ship_start_letter == ship_end_letter:
         if (int(ship_start_number) - int(ship_end_number) == ship_len - 1 or
                 int(ship_start_number) - int(ship_end_number) == -ship_len + 1):
-            return True
+            return 'horizontal'
     return False
+
+
+def get_coords(ship_start, ship_end, level):
+    alphabet = string.ascii_uppercase
+    ship_start_letter, ship_start_number = ship_start
+    ship_start_letter_index = alphabet.index(ship_start_letter)
+    ship_end_letter, ship_end_number = ship_end
+    ship_end_letter_index = alphabet.index(ship_end_letter)
+    ship_coords = []
+    if level == 'horizontal':
+        for coord in range(int(ship_start_number), int(ship_end_number) + 1):
+            ship_coords.append(ship_start_letter + str(coord))
+    else:
+        for coord in range(ship_start_letter_index, ship_end_letter_index + 1):
+            ship_coords.append(alphabet[coord] + str(ship_start_number))
+    return ship_coords
+
+
+def locate_ships(ship_start, ship_end, battlefield):
+    pass
 
 
 def place_ships(ships_to_place):
@@ -56,9 +76,10 @@ def place_ships(ships_to_place):
         while ship_placed < ships_to_place[ship_size]:
             print('Podaj pozycję statku o rozmiarze ', ship_size)
             start_pos, end_pos = input('Podaj pozycję początkową oraz końcową: ').upper().split()
-            if check_arrangement(start_pos, end_pos, ship_size):
+            vertical_or_horizontal = check_ship(start_pos, end_pos, ship_size)
+            if vertical_or_horizontal:
                 ship_placed += 1
-                print(start_pos, end_pos)
+                print(start_pos, end_pos, vertical_or_horizontal)
         ships_to_place.pop(ship_size)
 
 

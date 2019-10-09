f2341bbb331b7617e74efa755c7845bec72ef577
Mateusz Król <matek8952@gmail.com>
2019-10-09 10:56:48 +0200

    Added placig ships on board

--- main_game.py
+++ main_game.py
@@ -69,12 +69,11 @@ def check_ship_location(ship_coords, battlefield, level):
     alphabet = string.ascii_uppercase
     # Sprawdzanie, czy w tym miejscu nie ma już innego statku
     for coord in ship_coords:
-        row, col = coord[0], coord[1:]
-        if battlefield[row][int(col)] != '.':
+        row, col = coord[0], int(coord[1:])
+        if battlefield[row][col] != '.':
             return False
     # Sprawdzanie, czy w sąsiedztwie nie ma innego statku
     for coord in ship_coords:
-        row, col = coord[0], int(coord[1:])
         previous_row = alphabet[alphabet.index(row) - 1]
         next_row = alphabet[alphabet.index(row) + 1]
         if level == 'horizontal':
@@ -84,15 +83,15 @@ def check_ship_location(ship_coords, battlefield, level):
                     return False
                 # Sprawdzanie góra-środek-dół na początku statku
                 if 1 < col < 10 and coord == ship_coords[0]:
-                    if (battlefield[previous_row][col - 1]
-                            or battlefield[row][col - 1]
-                            or battlefield[next_row][col - 1]):
+                    if (battlefield[previous_row][col - 1] == 's'
+                            or battlefield[row][col - 1] == 's'
+                            or battlefield[next_row][col - 1] == 's'):
                         return False
                 # Sprawdzanie góra-środek-dół na końcu statku
                 if 1 < col < 10 and coord == ship_coords[-1]:
-                    if (battlefield[previous_row][col + 1]
-                            or battlefield[row][col + 1]
-                            or battlefield[next_row][col + 1]):
+                    if (battlefield[previous_row][col + 1 == 's']
+                            or battlefield[row][col + 1 == 's']
+                            or battlefield[next_row][col + 1] == 's'):
                         return False
             # Sprawdzanie, gdy statek jest przy lewej krawędzi planszy
             if row == 'A':
@@ -130,15 +129,18 @@ def check_ship_location(ship_coords, battlefield, level):
     return True
 
 
-def put_ship_on_board():
-    pass
+def put_ship_on_board(ship_coords, battlefield):
+    for coord in ship_coords:
+        row, col = coord[0], int(coord[1:])
+        battlefield[row][col] = 's'
+    return battlefield
 
 
 def place_ships(ships_to_place, battlefield):
     while ships_to_place:
         ship_size = list(ships_to_place.keys())[0]
-        ship_placed = 0
-        while ship_placed < ships_to_place[ship_size]:
+        ships_placed = 0
+        while ships_placed < ships_to_place[ship_size]:
             print('Podaj pozycję statku o rozmiarze ', ship_size)
             start_pos, end_pos = input('Podaj pozycję początkową oraz końcową: ').upper().split()
             vertical_or_horizontal = check_ship_level(start_pos, end_pos, ship_size)
@@ -147,10 +149,13 @@ def place_ships(ships_to_place, battlefield):
                 coords = get_coords(start_pos, end_pos, vertical_or_horizontal)
                 print(check_ship_location(coords, battlefield, vertical_or_horizontal))
                 if check_ship_location(coords, battlefield, vertical_or_horizontal):
-                    ship_placed += 1
+                    battlefield = put_ship_on_board(coords, battlefield)
+                    ships_placed += 1
+                print_map(battlefield)
         ships_to_place.pop(ship_size)
 
 
 if __name__ == "__main__":
-    print_map(generate_empty_map(10))
-    place_ships(generate_ships(), generate_empty_map(10))
+    ship_board = generate_empty_map(10)
+    print_map(ship_board)
+    place_ships(generate_ships(), ship_board)
